43f2319a7d0d51c722f5d3c7df3508c1ddfef360
Manuel <ruiz_r_manuel@hotmail.com>
2016-06-11 00:22:52 -0500

    Pelota

--- app/src/main/java/ceti/cocopong/Entidades/Pelota.java
+++ app/src/main/java/ceti/cocopong/Entidades/Pelota.java
@@ -4,4 +4,46 @@ package ceti.cocopong.Entidades;
  * Created by law on 11/06/16.
  */
 public class Pelota {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public Pelota(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
--- app/src/main/java/ceti/cocopong/Vistas/PongView.java
+++ app/src/main/java/ceti/cocopong/Vistas/PongView.java
@@ -1,7 +1,21 @@
 package ceti.cocopong.Vistas;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
 /**
  * Created by law on 11/06/16.
  */
-public class PongView {
+public class PongView extends SurfaceView{
+    public PongView(Context context) {
+        super(context);
+    }
+
+    public PongView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+
 }
